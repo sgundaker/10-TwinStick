5b767abdf04e4d3a34b67e60f044813b91fe216a
Samantha Gundaker <sam.gundaker@gmail.com>
2016-12-02 18:21:41 -0700

    Added script

---
+++ TwinStick/Assets/Message.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Message : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	"Arin got so mad at a game he pooped his pants once."
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
