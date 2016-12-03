b1fee6bb1e57fcfdc17f2b6037af01f001b6825f
Samantha Gundaker <sam.gundaker@gmail.com>
2016-12-02 18:22:53 -0700

    Edited message

--- TwinStick/Assets/Message.cs
+++ TwinStick/Assets/Message.cs
@@ -5,7 +5,7 @@ public class Message : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	"Arin got so mad at a game he pooped his pants once."
+	print ("Arin got so mad at a game he pooped his pants once.");
 	}
 	
 	// Update is called once per frame
